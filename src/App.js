bf3a12b5db447779eacf7bb08e0e6688df5c649a
slmutli <108276889+slmulti@users.noreply.github.com>
2023-03-30 12:52:04 +0100

    global state

--- src/App.js
+++ src/App.js
@@ -1,67 +1,15 @@
 import Header from "./components/Header";
-import FeedbackItem from "./components/FeedbackItem";
+import FeedbackList from "./components/FeedbackList";
+import { useState } from "react";
+import FeedbackData from "./data/feedbackData";
 
 function App() {
-    //===============================================================
-    //map through a list
-    //===============================================================
-    // const title = "Blog Post";
-    // const body = "this is my blog post";
-    // const comments = [
-    //     { id: 1, text: "comment one" },
-    //     { id: 2, text: "comment two" },
-    //     { id: 3, text: "comment three" },
-    // ];
-    // return (
-    //     <div className="container">
-    //         <h1>{title}</h1>
-    //         <p>{body}</p>
-    //         <div className="comments">
-    //             <h3>Comments ({comments.length})</h3>
-    //             <ul>
-    //                 {comments.map((comment, index) => (
-    //                     <li key={index}>{comment.text}</li>
-    //                 ))}
-    //             </ul>
-    //         </div>
-    //     </div>
-    // );
-    //===============================================================
-    //Ternierys
-    //===============================================================
-    // const title = "Blog Post";
-    // const body = "this is my blog post";
-    // const comments = [
-    //     { id: 1, text: "comment one" },
-    //     { id: 2, text: "comment two" },
-    //     { id: 3, text: "comment three" },
-    // ];
-    // const loading = false;
-    // const showComments = true;
-    // if (loading) return <h1>Loading....</h1>;
-    // const commentBlock = (
-    //     <div className="comments">
-    //         <h3>Comments ({comments.length})</h3>
-    //         <ul>
-    //             {comments.map((comment, index) => (
-    //                 <li key={index}>{comment.text}</li>
-    //             ))}
-    //         </ul>
-    //     </div>
-    // );
-    // return (
-    //     <div className="container">
-    //         <h1>{title}</h1>
-    //         <p>{body}</p>
-    //         {showComments && commentBlock}
-    //     </div>
-    // );
-    //===============================================================
+    const [feedback, setFeedback] = useState(FeedbackData);
     return (
         <>
             <Header />
             <div className="container">
-                <FeedbackItem />
+                <FeedbackList feedback={feedback} />
             </div>
         </>
     );
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,22 +1,11 @@
 import React from "react";
 import { useState } from "react";
 
-function FeedbackItem() {
-    const [rating, setRating] = useState(7);
-    const [text, setText] = useState("...This is an example of a feeback item");
-
-    const handleClick = () => {
-        setRating((prev) => {
-            console.log(prev);
-            return prev + 1;
-        });
-    };
-
+function FeedbackItem({ item }) {
     return (
         <div className="card">
-            <div className="num-display">{rating}</div>
-            <div className="text-display">{text}</div>
-            <button onClick={handleClick}>click</button>
+            <div className="num-display">{item.rating}</div>
+            <div className="text-display">{item.text}</div>
         </div>
     );
 }
