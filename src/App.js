491475e301d965b0d57bef826d5e5e73bc4e0208
slmutli <108276889+slmulti@users.noreply.github.com>
2023-03-30 12:06:16 +0100

    Components, Props and Prop Types

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
     //===============================================================
     return (
         <>
-            <Header text={true} />
+            <Header />
             <div className="container">
                 <h1>My App</h1>
             </div>
