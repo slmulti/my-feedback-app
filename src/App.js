da75c5702f449bf8031a51aca45a3c2c904c5a6e
slmutli <108276889+slmulti@users.noreply.github.com>
2023-03-30 11:57:37 +0100

    Components, Props and Prop Types

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //===============================================================
+    //map through a list
+    //===============================================================
+    // const title = "Blog Post";
+    // const body = "this is my blog post";
+    // const comments = [
+    //     { id: 1, text: "comment one" },
+    //     { id: 2, text: "comment two" },
+    //     { id: 3, text: "comment three" },
+    // ];
+    // return (
+    //     <div className="container">
+    //         <h1>{title}</h1>
+    //         <p>{body}</p>
+    //         <div className="comments">
+    //             <h3>Comments ({comments.length})</h3>
+    //             <ul>
+    //                 {comments.map((comment, index) => (
+    //                     <li key={index}>{comment.text}</li>
+    //                 ))}
+    //             </ul>
+    //         </div>
+    //     </div>
+    // );
+    //===============================================================
+    //Ternierys
+    //===============================================================
+    // const title = "Blog Post";
+    // const body = "this is my blog post";
+    // const comments = [
+    //     { id: 1, text: "comment one" },
+    //     { id: 2, text: "comment two" },
+    //     { id: 3, text: "comment three" },
+    // ];
+    // const loading = false;
+    // const showComments = true;
+    // if (loading) return <h1>Loading....</h1>;
+    // const commentBlock = (
+    //     <div className="comments">
+    //         <h3>Comments ({comments.length})</h3>
+    //         <ul>
+    //             {comments.map((comment, index) => (
+    //                 <li key={index}>{comment.text}</li>
+    //             ))}
+    //         </ul>
+    //     </div>
+    // );
+    // return (
+    //     <div className="container">
+    //         <h1>{title}</h1>
+    //         <p>{body}</p>
+    //         {showComments && commentBlock}
+    //     </div>
+    // );
+    //===============================================================
+    return (
+        <>
+            <Header text={true} />
+            <div className="container">
+                <h1>My App</h1>
+            </div>
+        </>
+    );
 }
 
 export default App;
